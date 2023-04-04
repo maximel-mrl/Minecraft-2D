9118fb2453b222abf89ea5a77bcf7ffe39e6bd8b
maxime-mrl <maxitrompette@ymail.com>
2023-04-04 23:37:38 +0200

    added terrain variation, need to split files next

--- index.js
+++ index.js
@@ -9,11 +9,23 @@ const block = {
     vCount: 5,
     dirt: document.createElement("img"),
     grass: document.createElement("img"),
+    stone: document.createElement("img"),
+    tallGrass: document.createElement("img"),
+    water: document.createElement("img"),
+    sand: document.createElement("img"),
+    sandStone: document.createElement("img"),
+    cactus: document.createElement("img"),
 }
 block.vCount = Math.round((canvas.height/block.s)*0.4)
 console.log(block.vCount)
 block.dirt.src = "./texture/dirt.jpg"
 block.grass.src = "./texture/grass.jpg"
+block.stone.src = "./texture/stone.jpg"
+block.tallGrass.src = "./texture/tallgrass.png"
+block.water.src = "./texture/water.jpg"
+block.sand.src = "./texture/sand.jpg"
+block.sandStone.src = "./texture/sandstone.jpg"
+block.cactus.src = "./texture/cactus.png"
 
 const world = {
     translateX: -80,
@@ -29,7 +41,9 @@ const world = {
             Math.random() * 0.3 + 0.25
         ]
     },
-    grassSeed: Math.round(Math.random() * 50),
+    stoneSeed: Math.random() * 20,
+    grassSeed: Math.random() * 5 + 10,
+    biomeSeed: Math.random() * 0.01 + 0.05,
 }
 
 let lastTime = 0;
@@ -53,7 +67,7 @@ function update() {
 }
 
 function updateBlocks(delay) {
-    world.translateX -= 500*delay*world.dir;
+    world.translateX -= 5000*delay*world.dir;
     if (world.translateX < -5* block.s) {
         world.translateX += block.s;
         world.blockPos++
@@ -65,21 +79,26 @@ function updateBlocks(delay) {
     updateFg(world.translateX, world.blockPos)
 }
 
-function drawBlock(img, x, y, mul=1) {
-    ctx.drawImage(img, 0, 0, block.imgS, block.imgS, x, y, block.s*mul, block.s*mul);
+function drawBlock(img, x, y, Ymul=1) {
+    ctx.drawImage(img, 0, 0, block.imgS, block.imgS, x, y + (1-Ymul)*block.s, block.s, block.s*Ymul);
     // draw image full: imgSRC, imgcropStart x-y, imgcropEnd x-y, posX, posY, SizeX, sizeY  
 }
-
+let biome
+setInterval(() => {
+    console.log(world)
+},500)
 function updateFg(offset, pos) {
     ctx.save()
     ctx.translate(offset, -block.s) // get to th bottom of block
     for (let i = 0; i < canvas.width/block.s*2 + block.s; i++) {
-        // BLOCK
+        /* ---------------------------------- BLOCK --------------------------------- */
         let hPos = Math.round((sinRnd(i+pos) + 1)* block.vCount * 0.5);
-        for (let y = 0; y < hPos; y++) {
-            drawBlock(block.dirt, i*block.s, canvas.height-y*block.s)
+        biome = sinRnd((i+pos)*world.biomeSeed);
+        if (biome >= 0) {
+            plainGeneration(i, hPos, pos)
+        } else {
+            desertGeneration(i, hPos, pos, biome)
         }
-        drawBlock(block.grass, i*block.s, canvas.height-hPos*block.s)
     }
     ctx.restore()
 }
@@ -103,10 +122,60 @@ document.addEventListener("keydown", ({key}) => {
             break;
     }
 })
-document.addEventListener("keyup", ({key}) => {
+document.addEventListener("keyup", () => {
     world.dir = 0;
 })
 
+function plainGeneration(i, hPos, pos, biome) {
+    let dirtHeight = Math.round((sinRnd((i+pos)*world.stoneSeed) + 2));
+    // stone
+    let y = 0;
+    if (hPos > dirtHeight) {
+        while (y < hPos - dirtHeight) { 
+            drawBlock(block.stone, i*block.s, canvas.height-y*block.s)
+            y++
+        }
+    }
+    while (y < hPos) { // dirt
+        drawBlock(block.dirt, i*block.s, canvas.height-y*block.s)
+        y++
+    }
+    // grass
+    drawBlock(block.grass, i*block.s, canvas.height-hPos*block.s)
+    // tallGrass
+    if (sinRnd((i+pos) * world.grassSeed) > 0.1) {
+        drawBlock(block.tallGrass, i*block.s, canvas.height-(hPos+1)*block.s)
+    }
+    // water
+    for (let y = hPos + 1; y < block.vCount/2; y++) {
+        ctx.clearRect(i*block.s, canvas.height-y*block.s, block.s, block.s) // remove grass if necessary
+        if (y + 1 < block.vCount/2) {
+            drawBlock(block.water, i*block.s, canvas.height-y*block.s)
+        }
+        drawBlock(block.water, i*block.s, canvas.height-y*block.s, 0.8)
+    }
+}
+
+function desertGeneration(i, hPos, pos) {
+    let sandHeight = Math.round((sinRnd((i+pos)*world.stoneSeed) + 4));
+    hPos = Math.round(hPos * 0.75 + block.vCount *0.25)
+    let y = 0;
+    if (hPos > sandHeight) {
+        while (y < hPos - sandHeight) { 
+            drawBlock(block.sandStone, i*block.s, canvas.height-y*block.s)
+            y++
+        }
+    }
+    while (y < hPos) { // dirt
+        drawBlock(block.sand, i*block.s, canvas.height-y*block.s)
+        y++
+    }
+    if (sinRnd((i+pos) * world.grassSeed) > 0.5) {
+        for (let z = 0; z <= Math.round(sinRnd((i+pos) * world.grassSeed + 2)*2.5); z++) {
+            drawBlock(block.cactus, i*block.s, canvas.height-(hPos+z)*block.s)
+        }
+    }
+}
 
 // test terrain generation to make sure it's good
 let message = "succes"
